58648ddbac066a0a3d0436277fa1d76396664b48
meletios <meletios@hedvig.com>
2019-08-28 13:17:49 +0200

    update pom, fix dependencied

--- src/main/kotlin/com/hedvig/rapio/service/QuoteServiceImpl.kt
+++ src/main/kotlin/com/hedvig/rapio/service/QuoteServiceImpl.kt
@@ -3,14 +3,16 @@ package com.hedvig.rapio.service
 import com.hedvig.rapio.data.QuoteRequestRepository
 import com.hedvig.rapio.data.entity.QuoteRequest
 import com.hedvig.rapio.web.dto.QuoteRequestDTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
-class QuoteServiceImpl(
+@Service
+class QuoteServiceImpl @Autowired constructor(
   val quoteRequestRepository: QuoteRequestRepository
 ) : QuoteService {
 
   override fun createQuote(requestDTO: QuoteRequestDTO) {
     quoteRequestRepository.save(QuoteRequest.from(requestDTO))
-
   }
 
 }
--- src/main/kotlin/com/hedvig/rapio/web/QuoteController.kt
+++ src/main/kotlin/com/hedvig/rapio/web/QuoteController.kt
@@ -1,6 +1,8 @@
 package com.hedvig.rapio.web
 
+import com.hedvig.rapio.service.QuoteService
 import com.hedvig.rapio.web.dto.QuoteRequestDTO
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("quote")
-class QuoteController() {
+class QuoteController @Autowired constructor(
+  val quoteService: QuoteService
+) {
 
   @PostMapping("create")
   fun createQuote(request: QuoteRequestDTO): ResponseEntity<Void> {
+    quoteService.createQuote(request)
 
     return ResponseEntity.accepted().build()
   }
