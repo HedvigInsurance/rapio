f9d59826a0bf00547bb909f2ef83a3a8fedd7df3
Johan Tjelldén <johan@hedvig.com>
2019-10-08 14:58:35 +0200

    Add mock to unittest

--- src/test/kotlin/com/hedvig/rapio/ApiKeysTest.kt
+++ src/test/kotlin/com/hedvig/rapio/ApiKeysTest.kt
@@ -1,9 +1,11 @@
 package com.hedvig.rapio
 
 import com.hedvig.rapio.comparison.QuoteService
+import com.hedvig.rapio.externalservices.underwriter.transport.UnderwriterClient
 import com.ninjasquad.springmockk.MockkBean
 import org.junit.Ignore
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -27,6 +29,9 @@ class ApiKeysTest {
 
     @MockkBean
     lateinit var quoteService: QuoteService
+
+    @MockkBean
+    lateinit var underwriterClient: UnderwriterClient
 
     @Ignore
     @Test
