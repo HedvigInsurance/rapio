dae782d637082edaf61e86ca9c60912c98866d3e
Johan Tjelldén <johan@hedvig.com>
2019-10-09 16:45:20 +0200

    Validate sign input parameters

--- src/main/kotlin/com/hedvig/rapio/comparison/web/dto/SignRequestDTO.kt
+++ src/main/kotlin/com/hedvig/rapio/comparison/web/dto/SignRequestDTO.kt
@@ -2,13 +2,18 @@ package com.hedvig.rapio.comparison.web.dto
 
 import java.time.LocalDate
 import java.time.ZoneId
+import javax.validation.Valid
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
 
 data class SignRequestDTO(
         val requestId:String,
-        val startsAt: Date,
-        val email:String,
-        val firstName: String,
-        val lastName: String
+        @get:Valid val startsAt: Date,
+
+        @get:Email val email:String,
+
+        @get:NotBlank val firstName: String,
+        @get:NotBlank val lastName: String
         )
 
 data class Date(
