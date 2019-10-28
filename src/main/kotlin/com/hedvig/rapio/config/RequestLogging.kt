e97dcb135a0c0a167c0f68dffa8bca1799d58bf1
Johan Tjelldén <johan@hedvig.com>
2019-10-28 16:09:03 +0100

    Add request logging

---
+++ src/main/kotlin/com/hedvig/rapio/config/RequestLogging.kt
@@ -0,0 +1,23 @@
+package com.hedvig.rapio.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.filter.CommonsRequestLoggingFilter
+
+
+@Profile("staging")
+@Configuration
+class RequestLoggingFilterConfig {
+
+    @Bean
+    fun logFilter(): CommonsRequestLoggingFilter {
+        val filter = CommonsRequestLoggingFilter()
+        filter.setIncludeQueryString(true)
+        filter.setIncludePayload(true)
+        filter.setMaxPayloadLength(10000)
+        filter.setIncludeHeaders(false)
+        filter.setAfterMessagePrefix("REQUEST DATA : ")
+        return filter
+    }
+}
