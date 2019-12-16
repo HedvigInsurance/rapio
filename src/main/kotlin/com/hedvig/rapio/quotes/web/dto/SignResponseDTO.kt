e1a24beaafe76e584cb8f70c580b52d0512c0727
Johan Tjelldén <johan@hedvig.com>
2019-12-16 16:45:36 +0100

    Add productId to sign response

--- src/main/kotlin/com/hedvig/rapio/quotes/web/dto/SignResponseDTO.kt
+++ src/main/kotlin/com/hedvig/rapio/quotes/web/dto/SignResponseDTO.kt
@@ -3,5 +3,6 @@ package com.hedvig.rapio.quotes.web.dto
 data class SignResponseDTO(
         val requestId: String,
         val quoteId: String,
+        val productId: String,
         val signedAt: Long
 )
