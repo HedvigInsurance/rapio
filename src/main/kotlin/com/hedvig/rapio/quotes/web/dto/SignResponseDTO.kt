950fb6ab1ba723ea2d0af5cc69b891e4bcef28ef
Johan Tjelldén <johan@hedvig.com>
2019-12-16 17:26:54 +0100

    Added a little deprecation annotation

--- src/main/kotlin/com/hedvig/rapio/quotes/web/dto/SignResponseDTO.kt
+++ src/main/kotlin/com/hedvig/rapio/quotes/web/dto/SignResponseDTO.kt
@@ -2,6 +2,7 @@ package com.hedvig.rapio.quotes.web.dto
 
 data class SignResponseDTO(
         val requestId: String,
+        @Deprecated("This has the same value as productId, so use that instead", level = DeprecationLevel.ERROR)
         val quoteId: String,
         val productId: String,
         val signedAt: Long
