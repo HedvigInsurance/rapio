7c205a85cafa39609a8b087814e4e073b1303dde
meletios <meletios@hedvig.com>
2020-09-15 11:04:59 +0200

    add product pricing client

---
+++ src/main/kotlin/com/hedvig/rapio/externalservices/productPricing/transport/ProductPricingClient.kt
@@ -0,0 +1,13 @@
+package com.hedvig.rapio.externalservices.productPricing.transport
+
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@FeignClient(name = "productPricingClient", url = "\${hedvig.productPricing.url:product-pricing}")
+interface ProductPricingClient {
+
+    @GetMapping(value = ["/_/contracts/members/{memberId}"])
+    fun getContractsByMemberId(@PathVariable("memberId") memberId: String): ResponseEntity<List<Contract>>
+}
