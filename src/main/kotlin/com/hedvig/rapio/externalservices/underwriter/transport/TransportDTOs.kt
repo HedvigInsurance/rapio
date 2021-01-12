48fa411af0106447ffbcbadeabce13a4d7f82235
Niklas Holmgren <niklas.holmgren@gmail.com>
2021-01-12 10:53:29 +0100

    Remove unused ApartmentProductSubType cases.

--- src/main/kotlin/com/hedvig/rapio/externalservices/underwriter/transport/TransportDTOs.kt
+++ src/main/kotlin/com/hedvig/rapio/externalservices/underwriter/transport/TransportDTOs.kt
@@ -9,12 +9,8 @@ import java.time.LocalDate
 enum class ApartmentProductSubType {
     BRF,
     RENT,
-    RENT_BRF,
-    SUBLET_RENTAL,
-    SUBLET_BRF,
     STUDENT_BRF,
     STUDENT_RENT,
-    LODGER,
     UNKNOWN
 }
 
