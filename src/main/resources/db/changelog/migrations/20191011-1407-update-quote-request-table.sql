b231e4a7a3ee3d33a45e56012c87b9fd8285ded0
vonElfvin <elvin.granat@gmail.com>
2021-04-23 11:14:39 +0200

    Clean up unused liquibase migrations so we can run it properly

--- src/main/resources/db/changelog/migrations/20191011-1407-update-quote-request-table.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted.sql
-
---changeset johant:20191011-1407-update-quote-request-table.sql logicalFilePath:migrations/20191011-1407-update-quote-request-table.sql
-
-ALTER TABLE "comparison_quote_request"
-    ADD COLUMN "valid_to" TIMESTAMP WITHOUT TIME ZONE;
-
---rollback ALTER TABLE "comparison_quote_request"
---rollback    DROP COLUMN "valid_to"
