df6086055ffb55a5c00398c32c3a2cbaaa90e822
Tobias Bexelius <tobiaslarsbexelius@gmail.com>
2021-05-14 11:54:54 +0200

    Update src/main/kotlin/com/hedvig/rapio/insuranceinfo/dto/IsMemberResponse.kt
    Co-authored-by: Elvin <elvin.granat@gmail.com>

--- src/main/kotlin/com/hedvig/rapio/insuranceinfo/dto/IsMemberResponse.kt
+++ src/main/kotlin/com/hedvig/rapio/insuranceinfo/dto/IsMemberResponse.kt
@@ -2,4 +2,7 @@ package com.hedvig.rapio.insuranceinfo.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class IsMemberResponse(@get:JsonProperty("isMember") val isMember:Boolean)
+data class IsMemberResponse(
+    @get:JsonProperty("isMember") 
+    val isMember:Boolean
+)
