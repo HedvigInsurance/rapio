f4d0dea8245ff800c3791775d4fdcdb750871de5
Tobias Bexelius <tobiaslarsbexelius@gmail.com>
2021-05-14 11:51:21 +0200

    Update src/main/kotlin/com/hedvig/rapio/externalservices/memberService/dto/IsMemberRequest.kt
    Co-authored-by: Elvin <elvin.granat@gmail.com>

--- src/main/kotlin/com/hedvig/rapio/externalservices/memberService/dto/IsMemberRequest.kt
+++ src/main/kotlin/com/hedvig/rapio/externalservices/memberService/dto/IsMemberRequest.kt
@@ -2,6 +2,6 @@ package com.hedvig.rapio.externalservices.memberService.dto
 
 data class IsMemberRequest(
     val memberId: String?,
-    val ssn: String?,
-    val email: String?
+    @Masked val ssn: String?,
+    @Masked val email: String?
 )
