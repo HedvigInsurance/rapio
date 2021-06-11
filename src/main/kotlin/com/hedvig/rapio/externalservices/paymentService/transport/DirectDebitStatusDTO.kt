8f715902fa6abf5984ee837308b497ffee633afe
Elvin <elvin.granat@gmail.com>
2021-06-11 10:09:08 +0200

    Include direct debit status in insurnace info dto (#55)

--- src/main/kotlin/com/hedvig/rapio/externalservices/paymentService/transport/DirectDebitStatusDTO.kt
+++ src/main/kotlin/com/hedvig/rapio/externalservices/paymentService/transport/DirectDebitStatusDTO.kt
@@ -2,5 +2,12 @@ package com.hedvig.rapio.externalservices.paymentService.transport
 
 data class DirectDebitStatusDTO(
     val memberId: String,
-    val directDebitActivated: Boolean
+    val directDebitActivated: Boolean,
+    val directDebitStatus: DirectDebitStatus
 )
+
+enum class DirectDebitStatus {
+    NEEDS_SETUP,
+    PENDING,
+    ACTIVATED
+}
